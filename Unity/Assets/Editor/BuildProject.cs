7142b7e786369c0b17fae9894fe4cdf4218c210d
mirocow <Mirocow@users.noreply.github.com>
2024-02-04 17:17:28 +0300

    Update BuildProject.cs
    Fixed some methods

--- Unity/Assets/Editor/BuildProject.cs
+++ Unity/Assets/Editor/BuildProject.cs
@@ -1,17 +1,33 @@
 // -------------------------------------------------------------------------------------------------
-// Assets/Editor/JenkinsBuild.cs
+// Assets/Editor/BuildProject.cs
 // -------------------------------------------------------------------------------------------------
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using UnityEditor.Build.Reporting;
+using System;
+using System.IO;
   
 // ------------------------------------------------------------------------
 // https://docs.unity3d.com/Manual/CommandLineArguments.html
+// "unity.exe" \
+//		-projectPath "C:/Users/Admin/Projects/MyProject" \
+//		-executeMethod BuildProject.BuildAndroid \
+//		-targetDir "C:\Users\Admin\Projects\Build"
 // ------------------------------------------------------------------------
-public class BuildProject {
+public class BuildProject 
+{
   
-    static string[] EnabledScenes = FindEnabledEditorScenes();
+    // ------------------------------------------------------------------------
+    // called from Jenkins
+    // ------------------------------------------------------------------------
+    public static void BuildAndroid()
+    {
+        var args = FindArgs();
+ 
+        string fullPathAndName = args.targetDir + args.appName + ".apk";
+        Build(fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.Android, BuildOptions.None);
+    }  
   
     // ------------------------------------------------------------------------
     // called from Jenkins
@@ -21,8 +37,16 @@ public class BuildProject {
         var args = FindArgs();
  
         string fullPathAndName = args.targetDir + args.appName + ".app";
-        Build(EnabledScenes, fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX, BuildOptions.None);
+        Build(fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX, BuildOptions.None);
     }
+	
+    public static void BuildiOS ()
+    {
+        var args = FindArgs();
+		
+        string fullPathAndName = args.targetDir + args.appName;
+        Build(fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.iOS, BuildOptions.None);
+    }	
  
     // ------------------------------------------------------------------------
     // called from Jenkins
@@ -32,7 +56,7 @@ public class BuildProject {
         var args = FindArgs();
  
         string fullPathAndName = args.targetDir + args.appName;
-        Build(EnabledScenes, fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64, BuildOptions.None);
+        Build(fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows, BuildOptions.None);
     }
  
     // ------------------------------------------------------------------------
@@ -43,48 +67,51 @@ public class BuildProject {
         var args = FindArgs();
  
         string fullPathAndName = args.targetDir + args.appName;
-        Build(EnabledScenes, fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.StandaloneLinux64, BuildOptions.None);
+        Build(fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.StandaloneLinux64, BuildOptions.None);
     }
- 
+
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------  
     private static Args FindArgs()
     {
         var returnValue = new Args();
- 
-        // find: -executeMethod
-        //   +1: JenkinsBuild.BuildMacOS
-        //   +2: FindTheGnome
-        //   +3: D:\Jenkins\Builds\Find the Gnome\47\output
-        string[] args = System.Environment.GetCommandLineArgs();
-        var execMethodArgPos = -1;
-        bool allArgsFound = false;
-        for (int i = 0; i < args.Length; i++)
-        {
-            if (args[i] == "-executeMethod")
-            {
-                execMethodArgPos = i;
-            }
-            var realPos = execMethodArgPos == -1 ? -1 : i - execMethodArgPos - 2;
-            if (realPos < 0)
-                continue;
- 
-            if (realPos == 0)
-                returnValue.appName = args[i];
-            if (realPos == 1)
-            {
-                returnValue.targetDir = args[i];
-                if (!returnValue.targetDir.EndsWith(System.IO.Path.DirectorySeparatorChar + ""))
-                    returnValue.targetDir += System.IO.Path.DirectorySeparatorChar;
- 
-                allArgsFound = true;
-            }
+		
+        string targetDir = GetArgByName("targetDir");
+        if(targetDir != null){
+            returnValue.targetDir = targetDir.TrimEnd(Path.DirectorySeparatorChar);;
         }
- 
-        if (!allArgsFound)
-            System.Console.WriteLine("[JenkinsBuild] Incorrect Parameters for -executeMethod Format: -executeMethod JenkinsBuild.BuildWindows <app name> <output dir>");
- 
+
+        returnValue.targetDir = returnValue.targetDir + Path.DirectorySeparatorChar;
+		
+        string gradleDir = GetArgByName("gradleDir");
+        if(gradleDir != null){
+            returnValue.gradleDir = gradleDir;
+        }
+
+        string appName = GetArgByName("appName");
+        if(appName != null){
+            returnValue.appName = appName;
+        }		
+  
         return returnValue;
     }
- 
+	
+    // ------------------------------------------------------------------------
+    // Helper function for getting the command line arguments
+    // ------------------------------------------------------------------------
+    private static string GetArgByName(string name)
+    {
+        var args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i].Contains(name) && args.Length > i + 1)
+            {
+                return args[i + 1];
+            }
+        }
+      
+        return null;
+    }	
  
     // ------------------------------------------------------------------------
     // ------------------------------------------------------------------------
@@ -97,16 +124,31 @@ public class BuildProject {
  
         return EditorScenes.ToArray();
     }
+
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------	
+    private static void ChangeGradle()
+    {
+		EditorPrefs.SetBool("JdkUseEmbedded", true);
+		EditorPrefs.SetBool("NdkUseEmbedded", true);
+		EditorPrefs.SetBool("SdkUseEmbedded", true);
+		EditorPrefs.SetBool("GradleUseEmbedded", false);
+		EditorPrefs.SetBool("AndroidGradleStopDaemonsOnExit", true);
+		
+		var args = FindArgs();
+        EditorPrefs.SetString("GradlePath", args.gradleDir);
+        System.Console.WriteLine("[JenkinsBuild] PreProcessBuild - changed path: "+EditorPrefs.GetString("GradlePath"));
+    }	
   
     // ------------------------------------------------------------------------
     // e.g. BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX
     // ------------------------------------------------------------------------
-    private static void Build(string[] scenes, string targetDir, BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions)
+    private static void Build(string outputPath, BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions)
     {
-        System.Console.WriteLine("[JenkinsBuild] Building:" + targetDir + " buildTargetGroup:" + buildTargetGroup.ToString() + " buildTarget:" + buildTarget.ToString());
+        System.Console.WriteLine("[JenkinsBuild] Building:" + outputPath + " buildTargetGroup:" + buildTargetGroup.ToString() + " buildTarget:" + buildTarget.ToString());
   
         // https://docs.unity3d.com/ScriptReference/EditorUserBuildSettings.SwitchActiveBuildTarget.html
-        bool switchResult = EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+        bool switchResult = EditorUserBuildSettings.SwitchActiveBuildTarget(buildTarget);
         if (switchResult)
         {
             System.Console.WriteLine("[JenkinsBuild] Successfully changed Build Target to: " + buildTarget.ToString());
@@ -116,9 +158,20 @@ public class BuildProject {
             System.Console.WriteLine("[JenkinsBuild] Unable to change Build Target to: " + buildTarget.ToString() + " Exiting...");
             return;
         }
+
+        if(buildTarget == BuildTarget.Android)
+        {
+            ChangeGradle();
+        }	
   
-        // https://docs.unity3d.com/ScriptReference/BuildPipeline.BuildPlayer.html
-        BuildReport buildReport = BuildPipeline.BuildPlayer(scenes, targetDir, buildTarget, buildOptions);
+        // https://docs.unity3d.com/ScriptReference/BuildPipeline.BuildPlayer.html		
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = FindEnabledEditorScenes();
+        buildPlayerOptions.locationPathName = outputPath;
+        buildPlayerOptions.target = buildTarget;
+        buildPlayerOptions.options = buildOptions;        
+        BuildReport buildReport = BuildPipeline.BuildPlayer(buildPlayerOptions);		
+		
         BuildSummary buildSummary = buildReport.summary;
         if (buildSummary.result == BuildResult.Succeeded)
         {
@@ -129,10 +182,58 @@ public class BuildProject {
             System.Console.WriteLine("[JenkinsBuild] Build Failed: Time:" + buildSummary.totalTime + " Total Errors:" + buildSummary.totalErrors);
         }		
     }
+
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------	
+    private static void BuildLocationPath(string outputFolder)
+    {
+        bool exists = System.IO.Directory.Exists(outputFolder);
+        if (!exists)
+            System.IO.Directory.CreateDirectory(outputFolder);
+    }	
+	
+    // ------------------------------------------------------------------------
+    /// Retrieves Application.persistentDataPath depends on the given platform.
+    /// 
+    /// iOS            - /var/mobile/Applications/[program_ID]/Documents : read/write
+    /// Android        - [External] /mnt/sdcard/Android/data/[bundle id]/files : read/write
+    ///                  [Internal] /data/data/[bundle id]/files/ : read/write
+    /// WEB Player     - /                
+    /// Windows Player - [UserDirectory]/AppData/LocalLow/[Company]/[Product Name] : read/write
+    /// OSX            - [UserDirectory]/Library/Caches/unity.[Company].[Product] : read/write
+    /// Windows Edtor  - [UserDirectory]/AppData/LocalLow/[Company]/[Product] : read/write
+    /// Mac Editor     - [UserDirectory]/Library/Caches/unity.[Company].[Product] : read/write
+    // ------------------------------------------------------------------------
+    public static string GetDocumentFilePath(string fileName)
+    {
+        string result = string.Empty;
+
+        if (Application.isEditor)
+        {
+            string datapath = "Assets";
+            string path = Application.dataPath.Substring(0, Application.dataPath.Length - datapath.Length);
+            result = Path.Combine(path, fileName);
+        }
+        else if( Application.platform == RuntimePlatform.WindowsPlayer )
+        {
+            result = Path.Combine( Application.dataPath, fileName );
+        }
+        else
+        {
+            result = Path.Combine(Application.persistentDataPath, fileName);
+        }
+
+        BuildLocationPath(result);
+
+        return result;
+    }
  
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
     private class Args
-    {		
+    {			
         public string appName = "AppName";
-        public string targetDir = Application.dataPath + "/" + "Output";
+        public string targetDir = GetDocumentFilePath("");
+        public string gradleDir = GetDocumentFilePath("gradle-6.7.1");
     }
 }
