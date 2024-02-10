ab2b7903e441dbcb792591119cbc60841d72e32d
mirocow <Mirocow@users.noreply.github.com>
2024-02-10 23:48:43 +0300

    Update GlobalContext.groovy

--- src/org/combine/GlobalContext.groovy
+++ src/org/combine/GlobalContext.groovy
@@ -17,23 +17,6 @@ class GlobalContext
     def errorMessage = ""
     def ArtifactsList = [] // Устанавливается при архивации артефактов, используется при нотификации
     def BranchName = "/main" // Устанавливается перед чекаутом
-
-    private final Script script
-
-    GlobalContext(Script script) {
-        this.script = script
-
-        if(this.script.params.APPNAME){
-            this.ApplicationName = this.script.params.APPNAME
-        }
-
-        if(this.script.params.WORKSPACE){
-            this.Workspace = this.script.params.WORKSPACE
-        }
-
-        if(!this.Workspace){
-            this.Workspace = this.script.pwd()
-        }
-    }	
+	
 }
 
