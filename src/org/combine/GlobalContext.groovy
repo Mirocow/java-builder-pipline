5d88b7bd71b9d49bf347882c6e5e52eb1248af0c
mirocow <Mirocow@users.noreply.github.com>
2024-02-10 19:52:27 +0300

    Update GlobalContext.groovy

--- src/org/combine/GlobalContext.groovy
+++ src/org/combine/GlobalContext.groovy
@@ -3,20 +3,39 @@ package org.combine
 
 class GlobalContext
 {
-	// Unity
-	def UnityFolder = 'C:\\Program Files\\Unity\\Hub\\Editor\\'
-	def MacUnityFolder = '/applications/unity/hub/editor/'
-	def UnityExecuteMethod = 'BuildProject.BuildAndroid'
-	def MacUnityVersion = '2022.3.17f1'
-	def OverrideUnityVersion = ''
-	def Workspace = ''
-	def ApplicationName = 'App'
 
-	// Переменные, устанавливаемые во время сборки
-	def ExecutingPlatform = "Windows"
-	def errorMessage = ""
-	def ArtifactsList = [] // Устанавливается при архивации артефактов, используется при нотификации
-	def BranchName = "/main" // Устанавливается перед чекаутом
+    private final Script script
+	
+    Builder(Script script) {
+        this.script = script
+    }
+	
+    // Unity
+    def UnityFolder = 'C:\\Program Files\\Unity\\Hub\\Editor\\'
+    def MacUnityFolder = '/applications/unity/hub/editor/'
+    def UnityExecuteMethod = 'BuildProject.BuildAndroid'
+    def MacUnityVersion = '2022.3.17f1'
+    def OverrideUnityVersion = ''
+    def Workspace = ''
+    def ApplicationName = 'App'
+	
+    // Переменные, устанавливаемые во время сборки
+    def ExecutingPlatform = "Windows"
+    def errorMessage = ""
+    def ArtifactsList = [] // Устанавливается при архивации артефактов, используется при нотификации
+    def BranchName = "/main" // Устанавливается перед чекаутом
+
+    if(script.params.APPNAME){
+        ApplicationName = script.params.APPNAME
+    }
+
+    if(script.params.WORKSPACE){
+        Workspace = script.params.WORKSPACE
+    }
+
+    if(!script.Workspace){
+        Workspace = script.pwd()
+    }	
 	
 }
 
